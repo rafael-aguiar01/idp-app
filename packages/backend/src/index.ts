831974c2c2b8a24f5595c0e3c88ecf1246328ab5
Rafael Aguiar <rafael.a1@hotmail.com>
2026-09-13 13:10:48 -0300

    feat: add Backstage Kubernetes plugin

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -22,6 +22,8 @@ backend.add(
 backend.add(import('@backstage/plugin-scaffolder-backend-module-azure'));
 backend.add(import('./modules/azurePipeline'));
 
+backend.add(import('@backstage/plugin-kubernetes-backend'));
+
 // techdocs plugin
 backend.add(import('@backstage/plugin-techdocs-backend'));
 
